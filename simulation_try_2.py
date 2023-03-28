fb8edcd1afcc493800914076f35c7a699acee155
גל חצור <ghatsor@gmail.com>
2023-03-28 22:15:33 +0300

    second commit

--- simulation_try_2.py
+++ simulation_try_2.py
@@ -28,15 +28,27 @@ z = -1 ### charge of the particle
 x_X_0 = uniform(1e-3,1e2) ### radiation length gcm^-2
 p_initial = 29.7885e6 # in eV
 Theta_0 = (13.6e6)/(beta*c*p_initial) *z*np.sqrt(x_X_0) * ( 1 + 0.038*math.log(x_X_0) )
-results = np.ones(100)
+results = np.ones((100,100))
 
 
 #%% Monte Carlo 
 temp = Theta_0
 
 for ii in range(0,len(results)):
-    results[ii] = temp;
-    temp = gauss(-3*temp, 3*temp)
+    for jj in range(0,len(results)):
+        results[ii][jj] = temp;
+        temp = gauss(0, temp);
+    temp = Theta_0
+
+results_mean =np.zeros(100)  
+for ii in range(0,len(results)):
+    count=np.zeros(100)  
+    for jj in range(0,len(results)):
+        count[jj] =results[ii][jj]
+    results_mean[ii] = np.mean(count)       
+#%% Some graphs, need to ask Enrique what
+
+        
     
     
     
