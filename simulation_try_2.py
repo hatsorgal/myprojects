ac2a0692b43d4fd1059881cad2e5d58fd97d1999
גל חצור <ghatsor@gmail.com>
2023-04-03 15:07:53 +0300

    third commit

--- simulation_try_2.py
+++ simulation_try_2.py
@@ -25,10 +25,10 @@ from random import uniform
 beta = 1
 c = 1 # eV
 z = -1 ### charge of the particle
-x_X_0 = uniform(1e-3,1e2) ### radiation length gcm^-2
-p_initial = 29.7885e6 # in eV
+x_X_0 = uniform(1e-3,1) ### radiation length gcm^-2
+p_initial = 100e9 # in eV
 Theta_0 = (13.6e6)/(beta*c*p_initial) *z*np.sqrt(x_X_0) * ( 1 + 0.038*math.log(x_X_0) )
-results = np.ones((100,100))
+results = np.ones((1000,1000))
 
 
 #%% Monte Carlo 
@@ -40,13 +40,23 @@ for ii in range(0,len(results)):
         temp = gauss(0, temp);
     temp = Theta_0
 
-results_mean =np.zeros(100)  
+results_mean =np.zeros(1000)  
 for ii in range(0,len(results)):
-    count=np.zeros(100)  
+    count=np.zeros(1000)  
     for jj in range(0,len(results)):
         count[jj] =results[ii][jj]
     results_mean[ii] = np.mean(count)       
 #%% Some graphs, need to ask Enrique what
+radiation_length = np.zeros(1000);
+for i in range(0,len(radiation_length)):
+    radiation_length[i] = x_X_0 * i;
+    
+plt.figure()
+plt.plot(radiation_length ,results_mean,'.', label = 'scatter to radiation length')
+plt.xlabel("$Radiation Length [m]$")
+plt.ylabel("Scatter")
+plt.legend()
+plt.grid()
 
         
     
